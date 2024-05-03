58b11c9c21c45fc69b405e30cab516187f287139
Aloksingh36 <162535425+Aloksingh36@users.noreply.github.com>
2024-05-03 10:05:31 +0545

    Create mathdemo.java

---
+++ mathdemo.java
@@ -0,0 +1,12 @@
+public class mathdemo {
+    public static void main(String[] args) {
+        System.out.println("Maximum number is:\t" + Math.max(6, 7));
+        System.out.println("Square root of y is:\t" + Math.sqrt(25));
+        System.out.println("Power of x and y is:\t" + Math.pow(2, 3));
+        System.out.println("Rounding value of:\t" + Math.round(35.4));
+        System.out.println("Value of cos 0\t" + Math.cos(0));
+        System.out.println("Ceiling Value of\t:" + Math.ceil(3.1));
+        System.out.println("Value of PI\t:" + Math.PI);
+        System.out.println("Log value of:\t" + Math.log(90));
+    }
+}
